6eb7730a6609c3077546dd8037df77100a1c57e0
666-RRR <155640143+666-RRR@users.noreply.github.com>
2024-02-23 12:55:09 +0900

    IndexServlet.javaを作成

--- src/tasklist/IndexServlet.java
+++ src/tasklist/IndexServlet.java
@@ -8,25 +8,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class IndexServlet
- */
-@WebServlet("/Index")
+@WebServlet("/index")
 public class IndexServlet extends HttpServlet {
-//    private static final long serialVersionUID = 1L;
-//
-//
-//    public IndexServlet() {
-//        super();
-//        // TODO Auto-generated constructor stub
-//    }
+        private static final long serialVersionUID = 1L;
 
 
-
-        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-            response.sendRedirect("index.html");
-        }
-
+    public IndexServlet() {
+        super();
+        // TODO Auto-generated constructor stub
     }
 
 
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+                // TODO Auto-generated method stub
+                response.getWriter().append("Served at: ").append(request.getContextPath());
+        }
+
+}
